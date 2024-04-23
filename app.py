2958a4818b0c7263257f7fef7fb8b9d7b66cd3b0
rhyden-kx <137869637+rhyden-kx@users.noreply.github.com>
2024-04-23 23:54:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1160,4 +1160,4 @@ def update_output(n_clicks, sentence, uploaded_contents):
     return ""
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0', port=8050)
