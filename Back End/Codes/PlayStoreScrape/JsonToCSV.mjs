bd5a98b0a7857fcfdde5931595faabdc82730c90
N313H <156909793+N313H@users.noreply.github.com>
2024-04-25 00:42:12 +0800

    Update JsonToCSV.mjs

--- Back End/Codes/PlayStoreScrape/JsonToCSV.mjs
+++ Back End/Codes/PlayStoreScrape/JsonToCSV.mjs
@@ -1,6 +1,15 @@
 import axios from 'axios';
 import { parse } from 'json2csv';
 import fs from 'fs';
+import path from 'path';
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const dataFolderPath = path.join(__dirname, '../../Data');
+
+// Create the data folder if it doesn't exist
+if (!fs.existsSync(dataFolderPath)) {
+  fs.mkdirSync(dataFolderPath, { recursive: true });
+}
 
 // Make a GET request to your localhost server
 axios.get('http://localhost:3000/api/apps/sg.com.gxs.app/reviews/')
@@ -34,9 +43,12 @@ axios.get('http://localhost:3000/api/apps/sg.com.gxs.app/reviews/')
     // Convert JSON data to CSV format
     const csvData = parse(jsonData, { fields });
 
-    // Write CSV data to a file
-    fs.writeFileSync('PlayStoreData.csv', csvData);
-    console.log('CSV file created successfully.');
+    // Define the full path for the CSV file using path.join
+    const filePath = decodeURIComponent(path.join(dataFolderPath, 'PlayStoreData.csv'));
+
+    // Write CSV data to the specified file path
+    fs.writeFileSync(filePath, csvData);
+    console.log('CSV file created successfully at:', filePath);
   })
   .catch(error => {
     console.error('Error fetching data:', error);
